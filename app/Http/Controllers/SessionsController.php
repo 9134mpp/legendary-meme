88fc9a06989f09c11c5b7bd5928dc55a974e7704
lzx <1050463330@qq.com>
2019-11-26 11:23:34 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,6 +28,11 @@ class SessionsController extends Controller
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
        }
-
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您以退出成功! ');
+        return redirect('login');
     }
 }
